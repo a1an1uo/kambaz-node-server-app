2be99710cd21380df671489919cfa40e11dba440
alanluo <luo.yiju@northeastern.edu>
2025-04-02 16:10:29 -0400

    5.3.4.2 bizzare bug

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
 import cors from "cors";
+import UserRoutes from "./Kambaz/Users/routes.js";
+import "dotenv/config";
+import session from "express-session";
 const app = express()
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    }
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
 // app.use((req, res, next) => {
 //     res.header('Cache-Control', 'no-store, must-revalidate')
 //     res.header('Pragma', 'no-cache')
 //     res.header('Expires', new Date(0).toUTCString())
 //     next()
 // })
+app.use(express.json({ limit: '50mb' }));
+
+UserRoutes(app);
+CourseRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
